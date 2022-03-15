d46aeafaebd167fa7c43d3042f74b64f21c47198
Bowswa <69088845+Bowswa@users.noreply.github.com>
2022-03-15 13:03:02 -0400

    Push test version

---
+++ experiments/games/pretzel-clicker/re-write/main.js
@@ -0,0 +1,30 @@
+/*
+    This game is made by Bowswa with massive help & insparation from Cookie Clicker!
+*/
+
+var VERSION = 0.009;
+var BETA = 0;
+
+/*
+    Misc. Helper Functions
+*/
+
+Game.elmID = function(id) {return document.getElementById(id);}
+
+Game.Choose = function(arr) {return arr[Math.floor(Math.random()*arr.length)];}
+
+/*
+    Game Initialization
+*/
+
+var Game={};
+
+/*
+    Game Launch
+*/
+
+Game.Launch = function() {
+  Game.elmID('gameSpace').innerHTML = '<span id="version">Version:' + VERSION + '</span>';
+}
+
+window.onload = Game.Launch;

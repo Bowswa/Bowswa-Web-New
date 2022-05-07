2a712cea64f079152d5735dab254e503c608df74
Bowswa <69088845+Bowswa@users.noreply.github.com>
2022-05-07 11:46:22 -0400

    Finish Datapacks Update by fixing merge
    -Fixed merge by removing re-added files
    -Fixed merge by putting files in correct places

--- vanillaplus/datapacks.js
+++ vanillaplus/datapacks.js
@@ -1,120 +1,130 @@
+//Use `` for multi line strings instead of the ''+ thing
 function oneThirteenPacks() {
-  thirteenContent =
-    '<p>Nothing Yet</p>';
-  document.getElementById("middleContent").innerHTML = thirteenContent;
-  document.getElementById("v1.13").setAttribute('class', 'open');
-  document.getElementById("v1.14").setAttribute('class', 'disabled');
-  document.getElementById("v1.15").setAttribute('class', 'disabled');
-  document.getElementById("v1.16").setAttribute('class', 'disabled');
-  document.getElementById("v1.17").setAttribute('class', 'disabled');
-  document.getElementById("v1.18").setAttribute('class', 'disabled');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneFourteenPacks() {
-  fourteenContent =
-    '<p>Nothing Yet</p>';
-  document.getElementById("middleContent").innerHTML = fourteenContent;
-  document.getElementById("v1.13").setAttribute('class', 'disabled');
-  document.getElementById("v1.14").setAttribute('class', 'open');
-  document.getElementById("v1.15").setAttribute('class', 'disabled');
-  document.getElementById("v1.16").setAttribute('class', 'disabled');
-  document.getElementById("v1.17").setAttribute('class', 'disabled');
-  document.getElementById("v1.18").setAttribute('class', 'disabled');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneFifteenPacks() {
-  fifteenContent =
-    '<p>Nothing Yet</p>';
-  document.getElementById("middleContent").innerHTML = fifteenContent;
-  document.getElementById("v1.13").setAttribute('class', 'disabled');
-  document.getElementById("v1.14").setAttribute('class', 'disabled');
-  document.getElementById("v1.15").setAttribute('class', 'open');
-  document.getElementById("v1.16").setAttribute('class', 'disabled');
-  document.getElementById("v1.17").setAttribute('class', 'disabled');
-  document.getElementById("v1.18").setAttribute('class', 'disabled');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneSixteenPacks() {
-  sixteenContent =
-    '<button class="accordion">Afk Display V1.1.1<span class="right-plus">+</span></button>' +
-    '<div class="panel">' +
-    'Afk Display V1.1.0 is a vanilla tweaks datapack. I added a small feature that i plan on expanding upon and have' +
-    ' released it in this update.<br>' +
-    '<a href="downloads/afk display v1.1.1 - 1.16.zip" download>Download Here!</a>' +
-    '</div>';
-  document.getElementById("middleContent").innerHTML = sixteenContent;
-  document.getElementById("v1.13").setAttribute('class', 'disabled');
-  document.getElementById("v1.14").setAttribute('class', 'disabled');
-  document.getElementById("v1.15").setAttribute('class', 'disabled');
-  document.getElementById("v1.16").setAttribute('class', 'open');
-  document.getElementById("v1.17").setAttribute('class', 'disabled');
-  document.getElementById("v1.18").setAttribute('class', 'disabled');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneSeventeenPacks() {
-  seventeenContent =
-    '<button class="accordion">Loot Boxes Gen3<span class="right-plus">+</span></button>' +
-    '<div class="panel">' +
-    'Loot Boxes Datapack adds a drop to fishing, mob kills, and more that generates random loot. Genetation Three' +
-    'is the latest version. Generation Two was never released due to many bugs & other issues.<br>' +
-    '<a href="downloads/1.17/[1.17.x]Generation Three Vanilla+ Loot Boxes.zip" download>Download Here!</a>' +
-    '</div>' +
-    '<br><br>' +
-    '<button class="accordion">Afk Display V1.1.1<span class="right-plus">+</span></button>' +
-    '<div class="panel">' +
-    'Afk Display V1.1.0 is a vanilla tweaks datapack. I added a small feature that i plan on expanding upon and have' +
-    ' released it in this update.<br>' +
-    '<a href="datapacks/afk display v1.1.1 - 1.17.zip" download>Download Here!</a>' +
-    '</div>';
-  document.getElementById("middleContent").innerHTML = seventeenContent;
-  document.getElementById("v1.13").setAttribute('class', 'disabled');
-  document.getElementById("v1.14").setAttribute('class', 'disabled');
-  document.getElementById("v1.15").setAttribute('class', 'disabled');
-  document.getElementById("v1.16").setAttribute('class', 'disabled');
-  document.getElementById("v1.17").setAttribute('class', 'open');
-  document.getElementById("v1.18").setAttribute('class', 'disabled');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneEighteenPacks() {
-  eighteenContent =
-    '<button class="accordion">Loot Boxes Gen3<span class="right-plus">+</span></button>' +
-    '<div class="panel">' +
-    'Loot Boxes Datapack adds a drop to fishing, mob kills, and more that generates random loot. Genetation Three' +
-    'is the latest version. Generation Two was never released due to many bugs & other issues.<br>' +
-    '<a href="downloads/1.17/[1.17.x]Generation Three Vanilla+ Loot Boxes.zip" download>Download Here!</a>' +
-    '</div>' +
-    '<br><br>' +
-    '<button class="accordion">Better Dragon Fight Vanilla+ V1.0.0<span class="right-plus">+</span></button>' +
-    '<div class="panel">' +
-    'The Better Dragon Fight Vanilla+ Pack is a pack that gives a new armor set and a new strategy to the ender dragon' +
-    'fight.' +
-    'Just one armor piece blows a full neterite set out of the water. However it is such a rare drop I wouldn\'t expect' +
-    'a drop any time soon.<br>' +
-    '<a href="downloads/1.18/[1.18.x] Better Dragon Fight V1.zip" download>Download Here!</a>' +
-    '<a href="downloads/1.18/[1.18.x]Better Dragon Fight V1 NULLSCAPE COMPATIBILITY.zip" download>Download NullScape Compat Here!</a>' +
-    '</div>';
-  document.getElementById("middleContent").innerHTML = eighteenContent;
-  document.getElementById("v1.13").setAttribute('class', 'disabled');
-  document.getElementById("v1.14").setAttribute('class', 'disabled');
-  document.getElementById("v1.15").setAttribute('class', 'disabled');
-  document.getElementById("v1.16").setAttribute('class', 'disabled');
-  document.getElementById("v1.17").setAttribute('class', 'disabled');
-  document.getElementById("v1.18").setAttribute('class', 'open');
-  document.getElementById("v1.19").setAttribute('class', 'disabled');
-  listener();
-};
-
-function oneNineteenPacks() {
+    thirteenContent =
+      '<p>Nothing Yet</p>';
+    document.getElementById("middleContent").innerHTML = thirteenContent;
+    document.getElementById("v1.13").setAttribute('class', 'open');
+    document.getElementById("v1.14").setAttribute('class', 'disabled');
+    document.getElementById("v1.15").setAttribute('class', 'disabled');
+    document.getElementById("v1.16").setAttribute('class', 'disabled');
+    document.getElementById("v1.17").setAttribute('class', 'disabled');
+    document.getElementById("v1.18").setAttribute('class', 'disabled');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneFourteenPacks() {
+    fourteenContent =
+      '<p>Nothing Yet</p>';
+    document.getElementById("middleContent").innerHTML = fourteenContent;
+    document.getElementById("v1.13").setAttribute('class', 'disabled');
+    document.getElementById("v1.14").setAttribute('class', 'open');
+    document.getElementById("v1.15").setAttribute('class', 'disabled');
+    document.getElementById("v1.16").setAttribute('class', 'disabled');
+    document.getElementById("v1.17").setAttribute('class', 'disabled');
+    document.getElementById("v1.18").setAttribute('class', 'disabled');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneFifteenPacks() {
+    fifteenContent =
+      '<p>Nothing Yet</p>';
+    document.getElementById("middleContent").innerHTML = fifteenContent;
+    document.getElementById("v1.13").setAttribute('class', 'disabled');
+    document.getElementById("v1.14").setAttribute('class', 'disabled');
+    document.getElementById("v1.15").setAttribute('class', 'open');
+    document.getElementById("v1.16").setAttribute('class', 'disabled');
+    document.getElementById("v1.17").setAttribute('class', 'disabled');
+    document.getElementById("v1.18").setAttribute('class', 'disabled');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneSixteenPacks() {
+    sixteenContent =
+      '<p>Nothing Yet</p>';
+    document.getElementById("middleContent").innerHTML = sixteenContent;
+    document.getElementById("v1.13").setAttribute('class', 'disabled');
+    document.getElementById("v1.14").setAttribute('class', 'disabled');
+    document.getElementById("v1.15").setAttribute('class', 'disabled');
+    document.getElementById("v1.16").setAttribute('class', 'open');
+    document.getElementById("v1.17").setAttribute('class', 'disabled');
+    document.getElementById("v1.18").setAttribute('class', 'disabled');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneSeventeenPacks() {
+    seventeenContent =
+      `<div class="cards">
+        <div onclick="showInfo('Afk Display+','This Tech Datapack is designed to visually let you know when someone is afk after 2.5 minutes. You can also toggle a message when you go afk.', 'https://bowswa.tk/games/datapacks/downloads/1.17/[1.17.x]Afk Display v1.1.1+.zip')" class="card">
+          <img class='img' src="icons/VT_AD.png">
+          <h1 class="title">Afk Display<br><br></h1>
+          <p class="version"><b>v1.1.1+</b></p>
+        </div>
+      </div>`;
+    document.getElementById("middleContent").innerHTML = seventeenContent;
+    document.getElementById("v1.13").setAttribute('class', 'disabled');
+    document.getElementById("v1.14").setAttribute('class', 'disabled');
+    document.getElementById("v1.15").setAttribute('class', 'disabled');
+    document.getElementById("v1.16").setAttribute('class', 'disabled');
+    document.getElementById("v1.17").setAttribute('class', 'open');
+    document.getElementById("v1.18").setAttribute('class', 'disabled');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneEighteenPacks() {
+    eighteenContent =
+      `<div class="cards">
+        <div onclick="showInfo('Afk Display+','This Tech Datapack is designed to visually let you know when someone is afk after 2.5 minutes. You can also toggle a message when you go afk.', 'https://bowswa.tk/games/datapacks/downloads/1.18/[1.18.x]Afk Display 1.1.2+.zip')" class="card">
+          <img class='img' src="icons/VT_AD.png">
+          <h1 class="title">Afk Display<br><br></h1>
+          <p class="version"><b>v1.1.2+</b></p>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="card">
+          <img class='img' src="icons/VT_TS.png">
+          <h1 class="title">Track Statistics<br><br></h1>
+          <p class="version"><b>COMING SOON!</b></p>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="card">
+          <img class='img' src="icons/VP_BEF.png">
+          <h1 class="title">Better Ender Fight<br><br></h1>
+          <p class="version"><b>COMING SOON!</b></p>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="card">
+          <img class='img' src="icons/VP_BEF.png">
+          <h1 class="title">Loot Boxes Generation 4<br><br></h1>
+          <p class="version"><b>COMING SOON!</b></p>
+        </div>
+      </div>
+      <div class="cards">
+        <div class="card">
+          <img class='img' src="icons/VP_BEF.png">
+          <h1 class="title">Crafting Tweaks<br><br></h1>
+          <p class="version"><b>COMING SOON!</b></p>
+        </div>
+      </div>`;
+      
+    document.getElementById("middleContent").innerHTML = eighteenContent;
+    document.getElementById("v1.13").setAttribute('class', 'disabled');
+    document.getElementById("v1.14").setAttribute('class', 'disabled');
+    document.getElementById("v1.15").setAttribute('class', 'disabled');
+    document.getElementById("v1.16").setAttribute('class', 'disabled');
+    document.getElementById("v1.17").setAttribute('class', 'disabled');
+    document.getElementById("v1.18").setAttribute('class', 'open');
+    document.getElementById("v1.19").setAttribute('class', 'disabled');
+    listener();
+  };
+  
+  function oneNineteenPacks() {
     nineteenContent =
       '<p>Nothing Yet! 1.19 is still in snapshots!</p>';
     document.getElementById("middleContent").innerHTML = nineteenContent;
@@ -127,22 +137,26 @@ function oneNineteenPacks() {
     document.getElementById("v1.19").setAttribute('class', 'open');
     listener();
   };
-
-function listener() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+  
+  function showInfo(title, info, url) {
+    Swal.fire({
+      title: title,
+      text: info,
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#848484',
+      confirmButtonText: 'Download',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Downloaded ' + title + '!',
+          'The pack has been downloaded. If the download does not start, click <a href="' + url + '">here</a>.',
+          'success',
+        )
+        window.location.href
+          = url;
       }
-    });
+    })
   }
-};
-
-window.onload = oneEighteenPacks();
+  window.onload = oneEighteenPacks();
